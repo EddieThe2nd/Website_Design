64e8c5c847ebff7de312977cec9f3c60e5394d2a
DrFeels <edsonmorty@gmail.com>
2023-05-26 16:55:34 +0200

    Latest Project

--- Fashion/Fashion.php
+++ Fashion/Fashion.php
@@ -5,142 +5,128 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fashion</title>
-    <link rel="stylesheet" href="./Fashion.css">
-    <script src="fashion.js"></script>
-
+    <link rel="stylesheet" href="./Fashion/Fashion.css">
 </head>
 <body>
+    <h1>Fashion Items</h1>
 
-    <div class="video_container">
-        <video autoplay muted loop id="myVideo">
-            <source src="4k ink drop black.mp4" type="video/mp4">
-        </video>
+    <div class="first-row">
+        <div class="product">
+            <img src="colorJumpPants.jpg" alt="Product 1">
+            <div class="name">Color Jump Pant</div>
+            <div class="price">R350</div>
+            <a href="#" class="add-to-cart">Add to Cart</a>
+          </div>
+        <div class="product">
+            <img style="height: 180px;" src="MensCreamJacket.jpg" >
+            <div class="name">Oversized Cream Jacket</div>
+            <div class="price">R29.99</div>
+            <a href="#" class="add-to-cart">Add to Cart</a>
+          </div>
+
+          <div class="product">
+            <img src="FADA_2022_BlackSlipOnDress.jpg" alt="Product 2">
+            <div class="name">Slip on Dress</div>
+            <div class="price">R200</div>
+            <a href="#" class="add-to-cart">Add to Cart</a>
+          </div>
+
+          <div class="product">
+            <img src="MensSet.jpg" alt="Product 2">
+            <div class="name">Vision Set</div>
+            <div class="price">R400</div>
+            <a href="#" class="add-to-cart">Add to Cart</a>
+          </div>
+
+          
+
+         
+      
+          
+            <div class="product">
+                <img src="FADA_2022_BlackUmbrellaDress.jpg" alt="Product 2">
+                <div class="name">Black Umbrella Dress</div>
+                <div class="price">R289</div>
+                <a href="#" class="add-to-cart">Add to Cart</a>
+              </div>
+
     </div>
 
-    <section>
-        <div class="header">
-            <h2>Fashion<h2>       
-        </div>
-    </section>
-    <br>
-    <br>
-        <!-- <h1 class="Heading">Welcome to the World of Art Re-Imagined</h1> -->
-    <section class="container content-section">
-        <div class="shop_items">
-            <div class="item">
-                <img src="./Fashion-Clothes/050d7331903304f276da85f2fca4cd33.jpg" alt="" class="img1">
-                <p class="item_title">MaXhosa Premium Blue</p>
-                <div class="price_button_cont">
-                    <span class="item_price">R580.69</span>
-                    <button class="add_button" type="button">Add to Cart</button>
-                </div>
-            </div>
-            <div class="item">
-                <img src="./Fashion-Clothes/40da5adc400bb2f3f02937dc4219fe6a.jpg" alt="" class="img1">
-                <p class="item_title">Mens' Ace Vanilla Jacket</p>
-                <div class="price_button_cont">
-                    <span class="item_price">R180.69</span>
-                    <button class="add_button" type="button">Add to Cart</button>
-                </div>
-            </div>
-            <div class="item">
-                <img src="./Fashion-Clothes/7be4f1bc40ed4fcf9fa78b62f428b725.webp" alt="" class="img1">
-                <p class="item_title">Africanasisty Red</p>
-                <div class="price_button_cont">
-                    <span class="item_price">R200.69</span>
-                    <button class="add_button" type="button">Add to Cart</button>
-                </div>
-            </div>
-            <div class="item">
-                <img src="./Fashion-Clothes/africa-kusini-mitindo-mavazi.jpg" alt="" class="img1">
-                <p class="item_title">Makoti Aqua Blue</p>
-                <div>
-                    <span class="item_price">R95.69</span>
-                    <button class="add_button" type="button">Add to Cart</button>
-                </div>
-            </div>
-            
-        </div>
-    </section>
-    <br>
-    <br>
-    <section class="container content-section">
-        <div class="shop_items">
-            <div class="item">
-                <img src="./Fashion-Clothes/c24851402effa6babfa2e7d467a658bc--designers.jpg" alt="" class="img1">
-                <p class="item_title">MotherLand by Randy</p>
-                <div>
-                    <span class="item_price">R70.69</span>
-                    <button class="add_button" type="button">Add to Cart</button>
-                </div>
-            </div>
-            <div class="item">
-                <img src="./Fashion-Clothes/c27dab23dc165cedf82474faad4ddf52--laurent-men-fashion.jpg" alt="" class="img1">
-                <p class="item_title">Mens' Black Suede Coat</p>
-                <div>
-                    <span class="item_price">R420.79</span>
-                    <button class="add_button" type="button">Add to Cart</button>
-                </div>
-            </div>
-            <div class="item">
-                <img src="./Fashion-Clothes/de18f50dd690625b77f68888711ed42d.jpg" alt="" class="img1">
-                <p class="item_title">Mens' Black Cotton Coat</p>
-                <div>
-                    <span class="item_price">R1000.0</span>
-                    <button class="add_button" type="button">Add to Cart</button>
-                </div>
-            </div>
-            <div class="item">
-                <img src="./Fashion-Clothes/R.png" alt="" class="img1">
-                <p class="item_title">MaXhosa Orange Two Piece</p>
-                <div>
-                    <span class="item_price">R1780.69</span>
-                    <button class="add_button" type="button">Add to Cart</button>
-                </div>
-            </div>
-            
-        </div>
-    </section>
-    <br>
-    <br>
-    <section class="container content-section">
-        <div class="shop_items">
-            <div class="item">
-                <img src="./Fashion-Clothes/A.jpg" alt="" class="img1">
-                <p class="item_title">Pablo Hoodie</p>
-                <div>
-                    <span class="item_price">R80.69</span>
-                    <button class="add_button" type="button">Add to Cart</button>
-                </div>
-            </div>
-            <div class="item">
-                <img src="./Fashion-Clothes/B.jpg" alt="" class="img1">
-                <p class="item_title">Pablo Hoodie(Special edition)</p>
-                <div>
-                    <span class="item_price">R70.69</span>
-                    <button class="add_button" type="button">Add to Cart</button>
-                </div>
-            </div>
-            <div class="item">
-                <img src="./Fashion-Clothes/C.jpg" alt="" class="img1">
-                <p class="item_title">Lost Hoodie</p>
-                <div>
-                    <span class="item_price">R50.69</span>
-                    <button class="add_button" type="button">Add to Cart</button>
-                </div>
-            </div>
-            <div class="item">
-                <img src="./Fashion-Clothes/D.jpg" alt="" class="img1">
-                <p class="item_title">Ladies' Lost Hoodie</p>
-                <div>
-                    <span class="item_price">R90.69</span>
-                    <button class="add_button" type="button">Add to Cart</button>
-                </div>
-            </div>
-        </div>
-    </section>
-        <br>
-        <br>
-        <br>
+    <div class="second_row">
+        <div class="product">
+            <img src="FADA_2022_CreamPaperDress.jpg" alt="Product 2">
+            <div class="name">The Tera Dress</div>
+            <div class="price">R500</div>
+            <a href="#" class="add-to-cart">Add to Cart</a>
+          </div>
+    
+          <div class="product">
+            <img src="FADA_2022_dangaree.jpg" alt="Product 2">
+            <div class="name">Black Dangaree</div>
+            <div class="price">R400</div>
+            <a href="#" class="add-to-cart">Add to Cart</a>
+          </div>
+    
+          <div class="product">
+            <img src="FADA_2022_PufferJacket.jpg" alt="Product 2">
+            <div class="name">Puffer Jacket</div>
+            <div class="price">R350</div>
+            <a href="#" class="add-to-cart">Add to Cart</a>
+          </div>
+    
+          <div class="product">
+            <img src="FADA_2022_Top.jpg" >
+            <div class="name">Black Puffed Vest</div>
+            <div class="price">R180</div>
+            <a href="#" class="add-to-cart">Add to Cart</a>
+          </div>
+          <div class="product">
+            <img src="FADA_2022_BlackDress.jpg" alt="Product 1">
+            <div class="name">Puff Skater Dress</div>
+            <div class="price">R350</div>
+            <a href="#" class="add-to-cart">Add to Cart</a>
+          </div>
+
+    </div>
+
+    <div class="third_row">
+        <div class="product">
+            <img src="MensJacket.jpg" alt="">
+            <div class="name">Retro Jacket</div>
+            <div class="price">R500</div>
+            <a href="#" class="add-to-cart">Add to Cart</a>
+          </div>
+    
+          <div class="product">
+            <img src="FADA_2022_BlackPuffedShoulderTop.jpg" alt="Product 2">
+            <div class="name">Basic Puff Top</div>
+            <div class="price">R70</div>
+            <a href="#" class="add-to-cart">Add to Cart</a>
+          </div>
+    
+         
+    
+          <div class="product">
+            <img src="FADA_2022_Top.jpg" >
+            <div class="name">Black Puffed Vest</div>
+            <div class="price">R180</div>
+            <a href="#" class="add-to-cart">Add to Cart</a>
+          </div>
+          
+
+          <div class="product">
+            <img src="FADA_2022_BlackSleevelessDress.jpg" alt="Product 2">
+            <div class="name">Sleeveless Dress</div>
+            <div class="price">R29.99</div>
+            <a href="#" class="add-to-cart">Add to Cart</a>
+          </div>
+          <div class="product">
+            <img src="MensSuitGreen.jpg" alt="Product 2">
+            <div class="name">Puffer Jacket</div>
+            <div class="price">R350</div>
+            <a href="#" class="add-to-cart">Add to Cart</a>
+          </div>
+
+    </div>
 </body>
 </html>
---
+++ FeaturedProductsImages/FeaturedProducts.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Featured Products</title>
+    <link rel="stylesheet" href="./FeaturedProducts.css">
+</head>
+<body>
+    <h1>Featured Products</h1>
+
+    <div class="product">
+      <img src="./TERA-03 sports shoe.png" alt="Product 1">
+      <div class="name">Elevate Sports Shoe</div>
+      <div class="price">R800</div>
+      <a href="#" class="add-to-cart">Add to Cart</a>
+    </div>
+  
+    <div class="product">
+      <img src="./educational blocks.png" alt="Product 2">
+      <div class="name">Educational Blocks</div>
+      <div class="price">R29.99</div>
+      <a href="#" class="add-to-cart">Add to Cart</a>
+    </div>
+
+    <div class="product">
+        <img src="./reusable pads.png" alt="Product 2">
+        <div class="name">Re-Usable Pads</div>
+        <div class="price">R29.99</div>
+        <a href="#" class="add-to-cart">Add to Cart</a>
+      </div>
+
+      <div class="product">
+        <img src="FADA_2022_HousePiece.jpg" alt="Product 2">
+        <div class="name">Product 2</div>
+        <div class="price">R29.99</div>
+        <a href="#" class="add-to-cart">Add to Cart</a>
+      </div>
+
+      <div class="product">
+        <img src="./FADA_2022_decoPiece.jpg" alt="Product 2">
+        <div class="name">Product 2</div>
+        <div class="price">R29.99</div>
+        <a href="#" class="add-to-cart">Add to Cart</a>
+      </div>
+
+      <div class="product">
+        <img src="product2.jpg" alt="Product 2">
+        <div class="name">Product 2</div>
+        <div class="price">R29.99</div>
+        <a href="#" class="add-to-cart">Add to Cart</a>
+      </div>
+
+      <div class="product">
+        <img src="product2.jpg" alt="Product 2">
+        <div class="name">Product 2</div>
+        <div class="price">R29.99</div>
+        <a href="#" class="add-to-cart">Add to Cart</a>
+      </div>
+
+      <div class="product">
+        <img src="product2.jpg" alt="Product 2">
+        <div class="name">Product 2</div>
+        <div class="price">R29.99</div>
+        <a href="#" class="add-to-cart">Add to Cart</a>
+      </div>
+
+      <div class="product">
+        <img src="product2.jpg" alt="Product 2">
+        <div class="name">Product 2</div>
+        <div class="price">R29.99</div>
+        <a href="#" class="add-to-cart">Add to Cart</a>
+      </div>
+
+      <div class="product">
+        <img src="product2.jpg" alt="Product 2">
+        <div class="name">Product 2</div>
+        <div class="price">R29.99</div>
+        <a href="#" class="add-to-cart">Add to Cart</a>
+      </div>
+  
+    <!-- Add more products here... -->
+    
+</body>
+</html>

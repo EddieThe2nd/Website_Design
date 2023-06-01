835897e87dc60cd90fb4a91f0abcdf5e48bcb84e
Oros707 <tokyongazi.q@gmail.com>
2023-06-02 00:38:44 +0200

    Tokyo: userhomepage explore feature

--- HomePage.php
+++ HomePage.php
@@ -138,48 +138,8 @@
         </div>
     </section>
 
-    <footer class="section-p1">
-        <div class="col">
-            <img style="width: 130px; height: 140px;" id="footer-Logo" src="./DesignEclatlogo2.png" alt="">
-            <h4>Contact</h4>
-            <p><strong>Address:</strong> Faculty of Art, Design and Architecture, Cottesloe, Johannesburg, 2092</p>
-            <p><strong>Phone:</strong> +27 11 559 4555</p>
-            <p><strong>Hours:</strong>09am-4pm, Mon - Fri</p>
-            </div>
+    <?php include "footer.php"?>
 
-        </div>
-
-        <div class="col">
-            <h4>About</h4>
-            <a href="#">About Us</a>
-            <a href="#">Delivery Information</a>
-            <a href="#">Privacy Policy</a>
-            <a href="#">Terms & Conditions</a>
-            <a href="#">Contact Us</a>
-
-
-        </div>
-
-        <div class="col">
-            <h4>My Account</h4>
-            <a href="#">Login</a>
-            <a href="#">View Cart</a>
-            <a href="#">Track My Order</a>
-            <a href="#">Help</a>
-
-
-        </div>
-
-        <div class="col payment">
-            <p>Secure Payment Gateways</p>
-            <img src="./icons/secure pg.png" alt="">   
-        </div>
-
-        <div class="copyright">
-            <p>© 2023 UJ-Design, All Rights Reserved</p>
-        </div>
-
-    </footer>
 
 
     <script type="text/javascript">

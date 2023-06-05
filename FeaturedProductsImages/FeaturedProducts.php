26013e61b8c99645da6615240091235cbf037d68
xtla1298 <letsepethatoandrew@gmail.com>
2023-06-05 17:32:16 +0200

    sonke

--- FeaturedProductsImages/FeaturedProducts.php
+++ FeaturedProductsImages/FeaturedProducts.php
@@ -13,72 +13,45 @@
     <div class="product">
       <img src="./TERA-03 sports shoe.png" alt="Product 1">
       <div class="name">Elevate Sports Shoe</div>
-      <div class="price">R800</div>
-      <a href="#" class="add-to-cart">Add to Cart</a>
+  
     </div>
   
     <div class="product">
       <img src="./educational blocks.png" alt="Product 2">
       <div class="name">Educational Blocks</div>
-      <div class="price">R29.99</div>
-      <a href="#" class="add-to-cart">Add to Cart</a>
+      
+      
     </div>
 
     <div class="product">
         <img src="./reusable pads.png" alt="Product 2">
         <div class="name">Re-Usable Pads</div>
-        <div class="price">R29.99</div>
-        <a href="#" class="add-to-cart">Add to Cart</a>
+        
+        
+      </div> <br>
+
+      <div class="product">
+        <img class="life"  src="FADA_2022_HousePiece.jpg" alt="Product 2">
+        <div class="name">Car Vices</div>
+        
+        
       </div>
 
       <div class="product">
-        <img src="FADA_2022_HousePiece.jpg" alt="Product 2">
-        <div class="name">Product 2</div>
-        <div class="price">R29.99</div>
-        <a href="#" class="add-to-cart">Add to Cart</a>
+        <img class="life" src="./FADA_2022_decoPiece.jpg" alt="Product 2">
+        <div class="name">Wavey Life</div>
+        
+      
       </div>
 
       <div class="product">
-        <img src="./FADA_2022_decoPiece.jpg" alt="Product 2">
-        <div class="name">Product 2</div>
-        <div class="price">R29.99</div>
-        <a href="#" class="add-to-cart">Add to Cart</a>
+        <img  class="life" src="scalpture.webp" alt="Product 2">
+        <div class="name">White Sculpture</div>
+      
+        
       </div>
 
-      <div class="product">
-        <img src="scalpture.webp" alt="Product 2">
-        <div class="name">Sculpture</div>
-        <div class="price">R1300</div>
-        <a href="#" class="add-to-cart">Add to Cart</a>
-      </div>
-
-      <div class="product">
-        <img src="product2.jpg" alt="Product 2">
-        <div class="name">Product 2</div>
-        <div class="price">R29.99</div>
-        <a href="#" class="add-to-cart">Add to Cart</a>
-      </div>
-
-      <div class="product">
-        <img src="product2.jpg" alt="Product 2">
-        <div class="name">Product 2</div>
-        <div class="price">R29.99</div>
-        <a href="#" class="add-to-cart">Add to Cart</a>
-      </div>
-
-      <div class="product">
-        <img src="product2.jpg" alt="Product 2">
-        <div class="name">Product 2</div>
-        <div class="price">R29.99</div>
-        <a href="#" class="add-to-cart">Add to Cart</a>
-      </div>
-
-      <div class="product">
-        <img src="product2.jpg" alt="Product 2">
-        <div class="name">Product 2</div>
-        <div class="price">R29.99</div>
-        <a href="#" class="add-to-cart">Add to Cart</a>
-      </div>
+     
   
     <!-- Add more products here... -->
     
